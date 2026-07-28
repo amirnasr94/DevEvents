1e374bb237e08f819e88b04c7e275244fc593ae0
amirnasr94 <amir.nasr72@gmail.com>
2026-07-28 19:21:50 +0330

    implement validation schema and his type to use in form and generate errors

---
+++ validation/validateCreateEventForm.ts
@@ -0,0 +1,47 @@
+import * as z from "zod";
+
+const MAX_FILE_SIZE = 5 * 1024 * 1024; // 5MB
+const ACCEPTED_IMAGE_TYPES = ["image/png", "image/jpeg", "image/jpg"];
+
+export const validateCreateEventForm = z.object({
+  title: z
+    .string()
+    .trim()
+    .min(1, { error: "Title is required." })
+    .max(100, { error: "Title must not exceed 100 characters." }),
+
+  date: z.string().min(1, { error: "Date is required." }),
+
+  time: z.string().min(1, { error: "Time is required." }),
+
+  location: z
+    .string()
+    .trim()
+    .min(1, { error: "Location is required." })
+    .max(200, { error: "Location must not exceed 200 characters." }),
+
+  mode: z.string().min(1, { error: "Please select an event mode." }),
+
+  image: z
+    .instanceof(File, { error: "Please upload an image." })
+    .refine((file) => ACCEPTED_IMAGE_TYPES.includes(file.type), {
+      error: "Only PNG, JPG, and JPEG images are allowed.",
+    })
+    .refine((file) => file.size <= MAX_FILE_SIZE, {
+      error: "Image size must be less than 5 MB.",
+    }),
+
+  tags: z
+    .array(z.string().trim().min(1))
+    .min(1, { error: "Please add at least one tag." }),
+
+  description: z
+    .string()
+    .trim()
+    .min(10, { error: "Description must be at least 10 characters." })
+    .max(1000, {
+      error: "Description must not exceed 1000 characters.",
+    }),
+});
+
+export type CreateEventForm = z.infer<typeof validateCreateEventForm>;
