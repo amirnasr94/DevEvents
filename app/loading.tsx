f0de5e4ff19f4cb7f40f31114c670a6975aff71b
amirnasr94 <amir.nasr72@gmail.com>
2026-07-30 22:53:51 +0330

    implemented loading.tsx

--- app/loading.tsx
+++ app/loading.tsx
@@ -1,3 +1,15 @@
+import Image from "next/image";
+
 export default function loading() {
-  return <div>loading</div>;
+  return (
+    <div className="h-full flex flex-col items-center justify-center">
+      <Image
+        src="/assets/icons/loading.svg"
+        alt="loading"
+        width={320}
+        height={320}
+      />
+      <p className="text-2xl text-primary font-semibold">Loding...</p>
+    </div>
+  );
 }
