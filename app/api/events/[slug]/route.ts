65dd2a52d4cd37e419ee933163a7ab1734dc4aa4
amirnasr94 <amir.nasr72@gmail.com>
2026-08-02 18:21:08 +0330

    implementd PUT route handler to edit event

--- app/api/events/[slug]/route.ts
+++ app/api/events/[slug]/route.ts
@@ -1,6 +1,7 @@
 import Evens, { IEvent } from "@/database/event.model";
 import connectDB from "@/lib/mongodb";
 import { NextRequest, NextResponse } from "next/server";
+import { v2 as cloudinary } from "cloudinary";
 
 type RouteParams = {
   params: Promise<{
@@ -88,6 +89,92 @@ export async function DELETE(_req: NextRequest, { params }: RouteParams) {
     return NextResponse.json(
       { message: "Event deleted successfully." },
       { status: 200 },
+    );
+  } catch (error) {
+    if (error instanceof Error) {
+      if (error.message.includes("MONGODB_URI")) {
+        return NextResponse.json(
+          {
+            message: "Database configuration Error",
+          },
+          { status: 500 },
+        );
+      }
+
+      return NextResponse.json(
+        { message: "Cannot delete Event" },
+        { status: 500 },
+      );
+    }
+    return NextResponse.json(
+      { message: "Accured an unexpected Error, Try Later!" },
+      { status: 500 },
+    );
+  }
+}
+
+export async function PUT(req: NextRequest, { params }: RouteParams) {
+  try {
+    await connectDB();
+
+    const { slug } = await params;
+    if (!slug || typeof slug !== "string" || slug.trim() === "") {
+      return NextResponse.json(
+        {
+          message: "Invalide or Missing slug Parameter",
+        },
+        { status: 400 },
+      );
+    }
+
+    const formData = await req.formData();
+    let event;
+    try {
+      event = Object.fromEntries(formData.entries());
+    } catch (error) {
+      return NextResponse.json(
+        { message: "Invalid Form Data!" },
+        { status: 400 },
+      );
+    }
+
+    const imageFile = formData.get("image") as File;
+    if (!imageFile)
+      return NextResponse.json(
+        { message: "Image File is Required!" },
+        { status: 400 },
+      );
+
+    const arrauBufer = await imageFile.arrayBuffer();
+    const buffer = Buffer.from(arrauBufer);
+
+    const uploadImageResult = await new Promise((resolve, reject) => {
+      cloudinary.uploader
+        .upload_stream({ resource_type: "image" }, (error, result) => {
+          if (error) return reject(error);
+          resolve(result);
+        })
+        .end(buffer);
+    });
+    event.image = (uploadImageResult as { secure_url: string }).secure_url;
+
+    console.log({ slug });
+
+    const response = await Evens.updateOne(
+      { slug: { $eq: slug } },
+      {
+        $set: { description: "amir nasr3" },
+      },
+    );
+    console.log(response);
+
+    if (response.modifiedCount === 0) {
+      return NextResponse.json({ message: "Update Failed." }, { status: 400 });
+    }
+
+    return NextResponse.json(
+      { message: "Update Seccessfully." },
+      { status: 201 },
     );
   } catch (error) {
     if (error instanceof Error) {
